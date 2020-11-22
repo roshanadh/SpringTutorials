99a0b5c47738a4f7ddcd2d083a881e61bb94b370
Roshan Adhikari <nahsorad@gmail.com>
2020-11-22 10:14:26 +0545

    Fix prototype, singleton labels in "Scopes & Autowired" project

--- config-by-annotations/Fortunes - Bean Scopes & Autowired/src/np/com/roshanadhikary/ioc/BeanLifeCycleDemo.java
+++ config-by-annotations/Fortunes - Bean Scopes & Autowired/src/np/com/roshanadhikary/ioc/BeanLifeCycleDemo.java
@@ -7,12 +7,12 @@ public class BeanLifeCycleDemo {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("BeanLifeCycleApplicationContext.xml");
 
-		// Singleton scoped beans
+		// Prototype-scoped bean
 		FootballCoach coach = context.getBean("footballCoach", FootballCoach.class);
 		System.out.println(coach.getDailyWorkout());
 		System.out.println(coach.getDailyFortune());
 
-		// Prototype scoped beans
+		// Singleton-scoped bean
 		CricketCoach cricketCoach = context.getBean("cricketCoach", CricketCoach.class);
 		System.out.println(cricketCoach.getDailyWorkout());
 		System.out.println(cricketCoach.getDailyFortune());
